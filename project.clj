561a84535e2f4139ebd298afcd787a76ba4263d9
Sergey <sergey@minorodata.com>
2023-09-14 14:26:51 +0200

    finished checkbox component and polishing

--- project.clj
+++ project.clj
@@ -5,17 +5,8 @@
             :url  "http://www.apache.org/licenses/LICENSE-2.0"}
 
   :dependencies
-  [[org.clojure/clojure "1.11.1"]
-   ;; always use "provided" for Clojure(Script)
-   [org.clojure/clojurescript "1.10.520" :scope "provided"]]
+  ;; always use "provided" for Clojure(Script)
+  [[org.clojure/clojurescript "1.10.520" :scope "provided"]]
 
   :source-paths
-  ["src/main"]
-
-  :plugins
-  [[lein-codox "0.10.8"]]
-
-  :codox
-  {:output-path "docs"
-   :language    :clojurescript
-   :metadata    {:doc/format :markdown}})
+  ["src/main"])
